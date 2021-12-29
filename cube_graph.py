376be1f947747655c2fda98f7d368a7516b88dd1
Václav Volhejn <vaclav.volhejn@gmail.com>
2021-12-29 22:51:35 +0100

    Vylepšit a refactorovat CubeGraph

--- cube_graph.py
+++ cube_graph.py
@@ -32,50 +32,209 @@ neighbors in the full graph!
 """
 
 
-class CubeGraph(util.RubikScene):  # MovingCameraScene):
+class UnzoomCubeGraph(util.RubikScene):
     def construct(self):
         self.next_section(skip_animations=False)
-        # self.camera.frame.save_state()
 
-        solved = RubiksCube(cubie_size=0.3)
+        solved_cube = RubiksCube(cubie_size=0.18)
 
-        nodes = [solved]
-        edges = []
-        can_be_outside = set()
-        anims_to_do = []
+        n_nodes, edges, anims_to_do, g = get_graph()
 
-        def new_cube(cube, move, shift, outside=False):
-            c = cube.copy().do_move(move).shift(shift)
-            nodes.append(c)
-            edges.append((cube, c))
-            if outside:
-                can_be_outside.add(len(nodes) - 1)
-            anims_to_do.append((cube, c, move))
-            return c
+        g.fade(1)
+        self.add(g)  # Necessary for zooming to work properly (for some reason)
 
-        def add_edge(c1, c2, move):
-            edges.append((c1, c2))
-            anims_to_do.append((c1, c2, move))
+        g2 = g.copy()
+        g2.set_color(GREEN)
 
-        # easy 4cyklus
-        r1 = new_cube(solved, "L", LEFT + DOWN)
-        r2 = new_cube(solved, "R", DOWN + RIGHT)
-        r3 = new_cube(r1, "R", RIGHT + DOWN)
-        add_edge(r2, r3, "L")
+        self.add(solved_cube)
+        cubes = {0: solved_cube}
+        cubes_on_scene = [solved_cube]
 
-        # r1 bude základem 6cyklu
-        # nejdriv levo prave steny
-        s1 = new_cube(solved, "L'", LEFT + UP)
-        s2 = new_cube(s1, "R2", LEFT)
-        # pak hordolni
-        s3 = new_cube(s2, "U2", LEFT)
-        s4 = new_cube(s3, "D2", LEFT)
-        # a ted z druhe strany, nejdriv hordolni
-        s5 = new_cube(r1, "U2", LEFT)
-        s6 = new_cube(s5, "D2", LEFT)
-        s7 = new_cube(s6, "L2", LEFT)
-        add_edge(s7, s4, "R2")
+        scale = 5
+        g.scale(scale, about_point=ORIGIN)
+        solved_cube.scale(scale, about_point=ORIGIN)
 
+        # musi odpovidat poctu cyklu v `while anims_to_do`
+        anim_steps = 12
+
+        def scale_updater(mobject, dt):
+            mobject.scale((1 / scale) ** (dt / anim_steps), about_point=ORIGIN)
+
+        # Do one second of scaling in advance
+        g.scale((1 / scale) ** (1 / anim_steps))
+
+        g.add_updater(scale_updater)
+        solved_cube.add_updater(scale_updater)
+
+        def move_updater_factory(start, end):
+            return lambda l: l.put_start_and_end_on(
+                start.get_center() + IN, end.get_center() + IN
+            )
+
+        while anims_to_do:
+            print(len(anims_to_do), "animations left")
+            anims = [c.animate.shift(ORIGIN) for c in cubes_on_scene]
+            active = set()
+            to_create = {}
+            leftover = []
+            for (c1, c2, move) in anims_to_do:
+                if c1 in cubes and not c1 in active:
+                    cnew = cubes[c1].copy()
+
+                    to_create[c2] = cnew
+
+                    line = Line(
+                        g[c1].get_center() + IN,
+                        g[c2].get_center() + IN,
+                        shade_in_3d=True,
+                        color=GRAY,
+                    )
+
+                    line.add_updater(move_updater_factory(cubes[c1], cnew))
+
+                    anims += [
+                        CubeMove(cnew, move=move, target_position=g[c2].get_center()),
+                        Create(line),
+                    ]
+                    cubes_on_scene.append(cnew)
+                    active.add(c1)
+                else:
+                    leftover.append((c1, c2, move))
+
+            self.play(*anims, run_time=1)
+            anims_to_do = leftover
+            cubes.update(to_create)
+
+        for cube in cubes_on_scene:
+            cube.clear_updaters()
+
+        self.wait(2)
+        return
+
+
+class HighlightCubeGraph(util.RubikScene):
+    def construct(self):
+        self.next_section(skip_animations=False)
+
+        solved_cube = RubiksCube(cubie_size=0.18)
+        n_nodes, edges, anims_to_do, g = get_graph()
+        g.fade(0)
+        for v in g.vertices.values():
+            v.fade(1)
+
+        self.add(g)
+
+        cubes = {0: solved_cube}
+        cubes_on_scene = [solved_cube]
+
+        while anims_to_do:
+            leftover = []
+            for (c1, c2, move) in anims_to_do:
+                if c1 in cubes:
+                    cnew = cubes[c1].copy()
+                    cnew.do_move(move).move_to(g[c2].get_center())
+
+                    cubes_on_scene.append(cnew)
+                    cubes[c2] = cnew
+                else:
+                    leftover.append((c1, c2, move))
+
+            anims_to_do = leftover
+
+        self.add(*cubes_on_scene)
+
+        # change to houses
+
+        self.next_section(skip_animations=False)
+
+        houses = [
+            ImageMobject("img/house.png").move_to(c.get_center())
+            for c in cubes_on_scene
+        ]
+
+        icons = [
+            ImageMobject("img/icon.png").move_to(c.get_center()) for c in cubes_on_scene
+        ]
+
+        infty = 10000.0
+        for c, house, icon in zip(cubes_on_scene, houses, icons):
+            house.width = icon.width = c.width
+            house.scale(1.0 / infty)
+            icon.scale(1.0 / infty)
+
+        # cubes disappear
+        self.play(*[cube.animate.scale(1 / infty) for cube in cubes_on_scene])
+        self.wait()
+
+        # houses appear and disappear
+        self.add(*houses)
+        self.play(*[house.animate().scale(infty) for house in houses])
+        self.wait()
+        self.play(*[house.animate().scale(0) for house in houses])
+        self.remove(*houses)
+
+        # icons appear and disappear
+
+        self.add(*icons)
+        self.play(*[icon.animate().scale(infty) for icon in icons])
+        self.wait()
+        self.play(*[icon.animate().scale(0) for icon in icons])
+        self.remove(*icons)
+
+        # cubes appear
+        self.play(*[cube.animate.scale(infty) for cube in cubes_on_scene])
+        self.wait()
+
+class BFSCubeGraph(util.RubikScene):
+    def construct(self):
+        self.next_section(skip_animations=False)
+
+        solved_cube = RubiksCube(cubie_size=0.18)
+        n_nodes, edges, anims_to_do, g = get_graph()
+        g.fade(0)
+        self.add(g)
+
+
+
+
+def get_graph():
+    n_nodes = 1
+    edges = []
+    anims_to_do = []
+
+    def new_cube(cube, move, _shift, _outside=False):
+        nonlocal n_nodes
+        n_nodes += 1
+
+        edges.append((cube, n_nodes - 1))
+        anims_to_do.append((cube, n_nodes - 1, move))
+
+        return n_nodes - 1
+
+    def add_edge(c1, c2, move):
+        edges.append((c1, c2))
+        anims_to_do.append((c1, c2, move))
+
+    # easy 4cyklus
+    solved = 0
+    r1 = new_cube(solved, "L", LEFT + DOWN)
+    r2 = new_cube(solved, "R", DOWN + RIGHT)
+    r3 = new_cube(r1, "R", RIGHT + DOWN)
+    add_edge(r2, r3, "L")
+
+    # r1 bude základem 6cyklu
+    # nejdriv levo prave steny
+    s1 = new_cube(solved, "L'", LEFT + UP)
+    s2 = new_cube(s1, "R2", LEFT)
+    # pak hordolni
+    s3 = new_cube(s2, "U2", LEFT)
+    s4 = new_cube(s3, "D2", LEFT)
+    # a ted z druhe strany, nejdriv hordolni
+    s5 = new_cube(r1, "U2", LEFT)
+    s6 = new_cube(s5, "D2", LEFT)
+    s7 = new_cube(s6, "L2", LEFT)
+    add_edge(s7, s4, "R2")
+    if True:
         # + kratky cyklus
         t0 = new_cube(r2, "R", UP + RIGHT)
         t1 = new_cube(t0, "L'", UP + LEFT)
@@ -136,140 +295,21 @@ class CubeGraph(util.RubikScene):  # MovingCameraScene):
         n18 = new_cube(n17, "L", RIGHT, True)
         n19 = new_cube(n17, "U'", LEFT, True)
 
-        # rozhazeni kostek do prostoru
-        positions = [c.get_center() / 1.0 for c in nodes]
-        posedges = [(nodes.index(u), nodes.index(v)) for u, v in edges]
+    g = Graph(
+        list(range(n_nodes)),
+        edges,
+        edge_config={
+            "color": GRAY,
+            "shade_in_3d": True,  # Needed to keep the edges behind the cube
+        },
+        vertex_config={
+            "color": GRAY,
+            "shade_in_3d": True,  # Needed to keep the edges behind the cube
+        },
+        layout="kamada_kawai",
+    )
+    g.rotate(angle=-25 * DEGREES)
+    g.scale_to_fit_height(7.5)
+    g.shift(-g.vertices[0].get_center())
 
-        # small score means graph is scattered
-        def score():
-            sc = 0.0
-
-            for i in range(len(positions)):
-                Delta = -1
-                if i in can_be_outside:
-                    Delta = 0
-
-                pos = positions[i]
-                if (
-                    pos[0] < -(7 + Delta)
-                    or pos[0] > (7 + Delta)
-                    or pos[1] < -(4 + Delta)
-                    or pos[1] > (4 + Delta)
-                ):
-                    return float("inf")
-
-            for i in range(len(positions)):
-                for j in range(i + 1, len(positions)):
-                    sc += 1 / (norm(positions[i] - positions[j]) ** 2)
-
-            for i, j in posedges:
-                sc -= 1 / (2 * (norm(positions[i] - positions[j]) ** 2))
-
-            return sc
-
-        # randomly shift nodes until the graph is scattered
-        if False:
-            if score() == float("inf"):
-                positions = [pos / 1.2 for pos in positions]
-
-            length = 100
-            for l in range(length):
-                i = random.randrange(0, len(positions))
-
-                if i == 0:
-                    continue
-                delta = 1 - l * 1.0 / length
-                move = np.array(
-                    [random.uniform(-delta, delta), random.uniform(-delta, delta), 0]
-                )
-                oldscore = score()
-
-                oldpos = positions[i].copy()
-                positions[i] += move
-                newscore = score()
-
-                if newscore > oldscore:
-                    positions[i] = oldpos
-
-        # animate creation of the graph
-
-        self.add(solved)
-
-        created = set()
-        created.add(solved)
-        cubes_on_scene = [solved]
-
-        while anims_to_do:
-            print(len(anims_to_do))
-            anims = []
-            active = set()
-            to_create = set()
-            leftover = []
-            for (c1, c2, move) in anims_to_do:
-                if c1 in created and not c1 in active:
-                    cnew = c1.copy()
-                    anims += [
-                        CubeMove(cnew, move=move, target_position=c2.get_center()),
-                        Create(Line(c1.get_center(), c2.get_center())),
-                    ]
-                    cubes_on_scene.append(cnew)
-                    active.add(c1)
-                    to_create.add(c2)
-                else:
-                    leftover.append((c1, c2, move))
-
-            self.play(*anims, run_time=1)
-            anims_to_do = leftover
-            created = created.union(to_create)
-
-        # change to houses
-
-        self.next_section(skip_animations=True)
-
-        houses = [
-            ImageMobject("img/house.png").move_to(c.get_center())
-            for c in cubes_on_scene
-        ]
-
-        icons = [
-            ImageMobject("img/icon.png").move_to(c.get_center()) for c in cubes_on_scene
-        ]
-
-        infty = 10000.0
-        for c, house, icon in zip(cubes_on_scene, houses, icons):
-            house.width = icon.width = c.width
-            house.scale(1.0 / infty)
-            icon.scale(1.0 / infty)
-
-        # cubes disappear
-        anims = []
-        for cube in cubes_on_scene:
-            anims.append(cube.animate().scale(1.0 / infty))
-
-        self.play(*anims)
-
-        # houses appear and disappear
-
-        self.add(*houses)
-
-        self.play(*[house.animate().scale(infty) for house in houses])
-
-        self.play(*[house.animate().scale(0) for house in houses])
-        self.remove(*houses)
-
-        # icons appear and disappear
-
-        self.add(*icons)
-
-        self.play(*[icon.animate().scale(infty) for icon in icons])
-
-        self.play(*[icon.animate().scale(0) for icon in icons])
-
-        self.remove(*icons)
-
-        # cubes appear
-        anims = []
-        for cube in cubes_on_scene:
-            anims.append(cube.animate().scale(infty))
-
-        self.play(*anims)
+    return n_nodes, edges, anims_to_do, g
